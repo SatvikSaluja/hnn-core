1b1646da2c5acd6b34a9927d397cfa4207f5c52a
SalujaSatvik <satviksaluja2507@gmail.com>
2026-07-16 12:39:25 +0000

    runscripts to compare case 2 and case 3

--- satvik-runscripts/trial.py
+++ satvik-runscripts/trial.py
@@ -1,38 +1,67 @@
-import pickle
-import numpy as np
-from hnn_core import jones_2009_model, simulate_dipole
+from hnn_core import neymotin_2020_model, simulate_dipole
+synapse_tree = {
+    "L2_basket": {
+        "soma": {
+            0.5: {"ampa": ["L2_pyramidal"],"gabaa": ["L2_basket"]}
+        }
+    },
+    "L2_pyramidal": {
+        "apical_oblique": {
+            0.5: {"nmda": ["L2_pyramidal"],"ampa": ["L2_pyramidal"]}
+        },
+        "basal_2": {
+            0.5: {"nmda": ["L2_pyramidal"],"ampa": ["L2_pyramidal"]}
+        },
+        "basal_3": {
+            0.5: {"nmda": ["L2_pyramidal"],"ampa": ["L2_pyramidal"]}
+        },
+        "soma": {
+            0.5: {"gabaa": ["L2_basket"],"gabab": ["L2_basket"]}
+        }
+    },
 
-net1 = jones_2009_model(orignal_synapse_creation=True)
-net2 = jones_2009_model(orignal_synapse_creation=False)
+    "L5_basket": {
+        "soma": {
+            0.5: {"gabaa": ["L5_basket"],"ampa": ["L5_pyramidal", "L2_pyramidal"]}
+        }
+    },
 
+    "L5_pyramidal": {
+        "apical_oblique": {
+            0.5: {"nmda": ["L5_pyramidal"],"ampa": ["L5_pyramidal", "L2_pyramidal"]}
+        },
+        "basal_2": {
+            0.5: {"nmda": ["L5_pyramidal"],"ampa": ["L5_pyramidal", "L2_pyramidal"]
+            }
+        },
+        "basal_3": {
+            0.5: {"nmda": ["L5_pyramidal"],"ampa": ["L5_pyramidal", "L2_pyramidal"]}
+        },
+        "soma": {
+            0.5: {"gabaa": ["L5_basket"],"gabab": ["L5_basket"]
+            }
+        },
+        "apical_tuft": {
+            0.5: {"ampa": ["L2_pyramidal"],"gabaa": ["L2_basket"]}
+        }
+    }
+    }
+net1 = neymotin_2020_model(orignal_synapse_creation=False,big_synapse_tree=synapse_tree)
 net1.add_evoked_drive('evprox', mu=40, sigma=5, numspikes=1,
     location='proximal',
     weights_ampa={'L2_pyramidal': 0.01, 'L5_pyramidal': 0.01},
-    synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},
-    event_seed=274)
+    synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},)
+net2 = neymotin_2020_model(orignal_synapse_creation=False,big_synapse_tree=None)
 net2.add_evoked_drive('evprox', mu=40, sigma=5, numspikes=1,
     location='proximal',
     weights_ampa={'L2_pyramidal': 0.01, 'L5_pyramidal': 0.01},
-    synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},
-    event_seed=274)
+    synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},)
+
+
 
 dpl1 = simulate_dipole(net1, tstop=170, record_vsec='all')
 dpl2 = simulate_dipole(net2, tstop=170, record_vsec='all')
-
-with open("net1_orignal.pkl", "wb") as f:
-    pickle.dump(net1, f)
-with open("net2_syanpse_tree.pkl", "wb") as f:
-    pickle.dump(net2, f)
-with open("dpl1_orignal.pkl", "wb") as f:
-    pickle.dump(dpl1, f)
-with open("dpl2_syanpse_tree.pkl", "wb") as f:
-    pickle.dump(dpl2, f)
-
-for key in dpl1[0].data.keys():
-    arr1 = np.asarray(dpl1[0].data[key])
-    arr2 = np.asarray(dpl2[0].data[key])
-    print(f" {key} ")
-    print(" equal:", np.array_equal(arr1, arr2))
-    print("allclose:", np.allclose(arr1, arr2))
-    print("max  diff:", np.max(np.abs(arr1 - arr2)))
-    print("mean diff:", np.mean(np.abs(arr1 - arr2)))
+import numpy as np
+for key in ['agg', 'L2', 'L5']:
+    diff = np.max(np.abs(dpl1[0].data[key] - dpl2[0].data[key]))
+    print(f"{key}: {diff}")
