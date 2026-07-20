5693b20acef51be8d910c47573c15d723e6431aa
SalujaSatvik <satviksaluja2507@gmail.com>
2026-07-20 07:05:42 +0000

    adding script file

--- satvik-runscripts/trial.py
+++ satvik-runscripts/trial.py
@@ -1,67 +1,104 @@
-from hnn_core import neymotin_2020_model, simulate_dipole
+from hnn_core import neymotin_2020_model
+from hnn_core.network_builder import NetworkBuilder
+
 synapse_tree = {
     "L2_basket": {
         "soma": {
-            0.5: {"ampa": ["L2_pyramidal"],"gabaa": ["L2_basket"]}
+            0.1: {
+                "ampa": ["L2_pyramidal"],
+                "gabaa": ["L2_basket"]
+            }
         }
     },
+
     "L2_pyramidal": {
         "apical_oblique": {
-            0.5: {"nmda": ["L2_pyramidal"],"ampa": ["L2_pyramidal"]}
+            0.1: {
+                "nmda": ["L2_pyramidal"],
+                "ampa": ["L2_pyramidal"]
+            }
         },
         "basal_2": {
-            0.5: {"nmda": ["L2_pyramidal"],"ampa": ["L2_pyramidal"]}
+            0.1: {
+                "nmda": ["L2_pyramidal"],
+                "ampa": ["L2_pyramidal"]
+            }
         },
         "basal_3": {
-            0.5: {"nmda": ["L2_pyramidal"],"ampa": ["L2_pyramidal"]}
+            0.1: {
+                "nmda": ["L2_pyramidal"],
+                "ampa": ["L2_pyramidal"]
+            }
         },
         "soma": {
-            0.5: {"gabaa": ["L2_basket"],"gabab": ["L2_basket"]}
+            0.1: {
+                "gabaa": ["L2_basket"],
+                "gabab": ["L2_basket"]
+            }
         }
     },
 
     "L5_basket": {
         "soma": {
-            0.5: {"gabaa": ["L5_basket"],"ampa": ["L5_pyramidal", "L2_pyramidal"]}
+            0.1: {
+                "gabaa": ["L5_basket"],
+                "ampa": ["L5_pyramidal", "L2_pyramidal"]
+            }
         }
     },
 
     "L5_pyramidal": {
         "apical_oblique": {
-            0.5: {"nmda": ["L5_pyramidal"],"ampa": ["L5_pyramidal", "L2_pyramidal"]}
+            0.1: {
+                "nmda": ["L5_pyramidal"],
+                "ampa": ["L5_pyramidal", "L2_pyramidal"]
+            }
         },
         "basal_2": {
-            0.5: {"nmda": ["L5_pyramidal"],"ampa": ["L5_pyramidal", "L2_pyramidal"]
+            0.1: {
+                "nmda": ["L5_pyramidal"],
+                "ampa": ["L5_pyramidal", "L2_pyramidal"]
             }
         },
         "basal_3": {
-            0.5: {"nmda": ["L5_pyramidal"],"ampa": ["L5_pyramidal", "L2_pyramidal"]}
+            0.1: {
+                "nmda": ["L5_pyramidal"],
+                "ampa": ["L5_pyramidal", "L2_pyramidal"]
+            }
         },
         "soma": {
-            0.5: {"gabaa": ["L5_basket"],"gabab": ["L5_basket"]
+            0.1: {
+                "gabaa": ["L5_basket"],
+                "gabab": ["L5_basket"]
             }
         },
         "apical_tuft": {
-            0.5: {"ampa": ["L2_pyramidal"],"gabaa": ["L2_basket"]}
+            0.1: {
+                "ampa": ["L2_pyramidal"],
+                "gabaa": ["L2_basket"]
+            }
         }
     }
-    }
-net1 = neymotin_2020_model(orignal_synapse_creation=False,big_synapse_tree=synapse_tree)
-net1.add_evoked_drive('evprox', mu=40, sigma=5, numspikes=1,
-    location='proximal',
-    weights_ampa={'L2_pyramidal': 0.01, 'L5_pyramidal': 0.01},
-    synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},)
-net2 = neymotin_2020_model(orignal_synapse_creation=False,big_synapse_tree=None)
-net2.add_evoked_drive('evprox', mu=40, sigma=5, numspikes=1,
-    location='proximal',
-    weights_ampa={'L2_pyramidal': 0.01, 'L5_pyramidal': 0.01},
-    synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},)
+}
+
+net = neymotin_2020_model(
+    orignal_synapse_creation=False,
+    big_synapse_tree=synapse_tree,
+)
+
+builder = NetworkBuilder(net)
 
 
+for cell_type in net.gid_ranges:
+    gid = list(net.gid_ranges[cell_type])[0]  #we only take one cell , as all cells have a copy of synapse_tree so 
+    #all cell will have same location
+    cell = builder._cells[gid]
+    cell_secs = set(cell._nrn_sections.values())
 
-dpl1 = simulate_dipole(net1, tstop=170, record_vsec='all')
-dpl2 = simulate_dipole(net2, tstop=170, record_vsec='all')
-import numpy as np
-for key in ['agg', 'L2', 'L5']:
-    diff = np.max(np.abs(dpl1[0].data[key] - dpl2[0].data[key]))
-    print(f"{key}: {diff}")
+    print(f"\n{cell_type} (gid {gid}):")
+    for conn_key, ncs in builder.ncs.items():
+        for nc in ncs:
+            seg = nc.syn().get_segment()
+            if seg.sec in cell_secs:
+                sec_name = seg.sec.name().split('.')[-1]
+                print(f"  {conn_key}: sec={sec_name}, loc={seg.x}")
