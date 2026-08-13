56aa89618af37781e8c788e76678c18d770d4ee8
SalujaSatvik <satviksaluja2507@gmail.com>
2026-08-13 10:27:24 +0000

    script file to check dataframe len and synapses len

--- satvik-runscripts/script.py
+++ satvik-runscripts/script.py
@@ -1,10 +1,15 @@
-from hnn_core import neymotin_2020_model
+from hnn_core import neymotin_2020_model, simulate_dipole
 
 net = neymotin_2020_model(use_data_frame=True)
 
-net.add_evoked_drive('evprox', mu=40, sigma=5, numspikes=1,
-        location='proximal',
-        weights_ampa={'L2_pyramidal': 0.01, 'L5_pyramidal': 0.01},
-        synaptic_delays={'L2_pyramidal': 0.1, 'L5_pyramidal': 0.1},)
+print(net.conn_dataframe)# if u add drives , the number of rows ( depicting one connection ), will increase
+# however the number of synapses doesnt increase
+dpls=simulate_dipole(net,tstop=170,dt=0.025)
+from neuron import h
+def count_neuron_synapses():
+    print(len(h.List("Exp2Syn")))
 
-print(net.conn_dataframe)
+    # we can print it also
+    #for x in h.List("Exp2Syn"):
+    #    print(x)
+count_neuron_synapses()
